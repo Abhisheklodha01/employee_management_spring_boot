be467e3621a10d47239f52577996454c2cbb0918
Abhishek lodha <119124845+Abhisheklodha01@users.noreply.github.com>
2025-02-14 23:02:16 +0530

    created employee class

--- src/main/java/com/abhisheklodha/Employee_management/Employee.java
+++ src/main/java/com/abhisheklodha/Employee_management/Employee.java
@@ -1,6 +1,14 @@
 package com.abhisheklodha.Employee_management;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Employee {
+    private Long id;
     private String name;
     private String PhoneNumber;
     private String email;
